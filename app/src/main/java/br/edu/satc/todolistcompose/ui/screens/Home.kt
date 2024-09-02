cdf82c7c810f1d82e37335d796f4abfbcbd946c6
Thyerri Mezzari <thyerrimezzari@hotmail.com>
2024-09-02 09:40:43 -0300

    ajuste na altura da column principal

--- app/src/main/java/br/edu/satc/todolistcompose/ui/screens/Home.kt
+++ app/src/main/java/br/edu/satc/todolistcompose/ui/screens/Home.kt
@@ -5,14 +5,13 @@ package br.edu.satc.todolistcompose.ui.screens
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.rounded.Add
 import androidx.compose.material.icons.rounded.Settings
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -30,7 +29,6 @@ import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -39,7 +37,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
-import androidx.compose.ui.layout.VerticalAlignmentLine
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import br.edu.satc.todolistcompose.TaskData
@@ -132,8 +129,8 @@ fun HomeScreen() {
 fun HomeContent(innerPadding: PaddingValues) {
 
     val tasks = mutableListOf<TaskData>()
-    for (i in 0..10) {
-        tasks.add(TaskData("Titulo " + i, "Descricao " + i, i % 2 == 0))
+    for (i in 0..5) {
+        tasks.add(TaskData("Tarefa " + i, "Descricao " + i, i % 2 == 0))
     }
 
     /**
@@ -148,7 +145,7 @@ fun HomeContent(innerPadding: PaddingValues) {
         modifier = Modifier
             .padding(horizontal = 4.dp)
             .padding(top = innerPadding.calculateTopPadding())
-            .fillMaxWidth()
+            .fillMaxSize()
             .verticalScroll(
                 rememberScrollState()
             ),
